1ec69a4903eb2197788d3f40d1f8f76b7617a6aa
Peter Hansen <mail@peterhansen.io>
2024-12-31 22:34:44 +0000

    Refactor Example Manuscript into __main__

--- src/Experiments/VoiceOver/__init__.py
+++ src/Experiments/VoiceOver/__init__.py
@@ -31,128 +31,7 @@ def load_cfg_from_env(env: Mapping[str, str]) -> Cfg:
     'ElevenLabsAPIKey': elvn.load_api_key_from_env(env),
     'WorkCache': pathlib.Path(env.get('WORK_CACHE', env.get('TMPDIR', './')).rstrip('/')) / 'elevenlabs',
   }
-
-async def run(
-  cfg: Cfg,
-  quit_event: asyncio.Event,
-  seed: int | bytes | None = None
-):
   
-  ### Setup consumables
-  vocies_cache_file = cfg['WorkCache'] / 'voices.json'
-  logger.debug('Establishing an API Session w/ ElevenLabs')
-  async with elvn.api_session(
-    api_key=cfg['ElevenLabsAPIKey'],
-  ) as api_session:
-    if vocies_cache_file.exists():
-      logger.debug('Loading the ElevenLabs Voices Cache')
-      voices = json.loads(vocies_cache_file.read_bytes())
-    else:
-      logger.debug('Caching the ElevenLabs Voices')
-      voices = await elvn.voices(api_session)
-      vocies_cache_file.write_bytes(json.dumps(voices).encode())
- 
-    ### Generate the Runtime Tools Protocol
-    @dataclass
-    class RuntimeTools:
-
-      ### Caching
-      (cfg['WorkCache'] / 'rt-cache').mkdir(exist_ok=True)
-      cache: pathlib.Path = field(default=cfg['WorkCache'] / 'rt-cache')
-      def key(self, *k: str) -> str: return hashlib.md5('|'.join(k).encode()).hexdigest()
-      def exists(self, *k: str) -> bool: return (self.cache / self.key(*k)).exists()
-      def slurp(self, *k: str) -> bytes: return (self.cache / self.key(*k)).read_bytes()
-      def dump(self, v: bytes, *k: str): (self.cache / self.key(*k)).write_bytes(v)
-      async def load(self, *k: str, chunk_size: int = CHUNK_SIZE) -> AsyncGenerator[BinaryIO, None]:
-        with (self.cache / self.key(*k)).open('rb') as f:
-          while (c := f.read(chunk_size)): yield c
-      async def save(self, *k: str, src: AsyncGenerator[ByteString, None]):
-        with (self.cache / self.key(*k)).open('wb') as f:
-          async for c in src: f.write(c)
-
-      ### Tool Protocol
-      async def dub(self, text: str, actor: str, sink: BinaryIO | None = None) -> bytes | None:
-        
-        ### Cache Results on a Cache Miss
-        if not self.exists(text, actor): self.save(text, actor, (
-          await elvn.tts(text, voices[actor], api_session, chunk_size=CHUNK_SIZE)
-        ))
-        assert self.exists(text, actor)
-
-        ### Load the Results from Cache
-        if sink is None: return self.slurp(text, actor)
-        else:
-          async for c in self.load(text, actor):
-            sink.write(c)
-
-    TOOLS = RuntimeTools()
-    
-    ### Build the Scene
-
-    actors = {
-      n: Actor(n) for n in (
-        'Lily', 'Roger', 'Callum'
-      )
-    }
-    roles = {
-      n: Role(n) for n in (
-        'Narrator', 'Protagonist', 'Antagonist'
-      )
-    }
-    castings = Castings(
-      roles=set(roles.values()),
-      actors=set(actors.values()),
-      castings={
-        roles[r]: actors[a]
-        for r, a in {
-          'Narrator': 'Lily',
-          'Protagonist': 'Roger',
-          'Antagonist': 'Callum',
-        }.items()
-      }
-    )
-
-    def compositor_factory(roles: set[Role]) -> Compositor:
-      compositor = Compositor()
-      for role in roles:
-        assert isinstance(role.name, str), role.name
-        compositor.add_layer(role.name)
-      return compositor
-
-    intro_factory: Callable[[str], str] = lambda k: f'Hello! My name is {castings.actor_by_role(k).name} and I play the role of {castings[k].name}.'
-
-    manuscript = Manuscript().add_act().add_scene(
-      'Example', 1, Scene(
-        name='Example',
-        castings=castings,
-        chronology=[
-          set( SceneEvent(when=when, **e) for e in events ) for when, events in enumerate((
-            (
-              { 'role': castings['Narrator'], 'voice_line': 'Scene Start.' },
-            ),
-            (
-              { 'role': castings['Narrator'], 'voice_line': intro_factory('Narrator') },
-            ),
-            (
-              { 'role': castings['Protagonist'], 'voice_line': intro_factory('Protagonist') },
-              { 'role': castings['Antagonist'], 'voice_line': intro_factory('Antagonist') },
-            ),
-            (
-              { 'role': castings['Narrator'], 'voice_line': 'Scene End.' },
-            )
-          ))
-        ],
-        compositor=compositor_factory(castings.roles),
-        tools=TOOLS,
-    ))
-
-    ### Render the Manuscript
-    
-    manuscript_workdir = cfg['WorkCache'] / 'manuscript'
-    manuscript_workdir.mkdir(exist_ok=True)
-    logger.info(f'Rendering Manuscript under {manuscript_workdir.as_posix()}')
-    await manuscript.render(manuscript_workdir)
-
 class Tools(Protocol):
 
   async def dub(self, text: str, actor: str, sink: BinaryIO | None = None) -> bytes | None: ...
@@ -592,3 +471,18 @@ class Manuscript:
 @dataclass
 class ShortStory:
   """The Original Short Story"""
+
+### Avoid Circular Imports
+
+from . import (
+  ElevenLabs as elvn,
+)
+
+__all__ = [
+  'Cfg', 'load_cfg_from_env',
+  'Tools',
+  'Actor', 'Role', 'Castings'
+  'VoiceLine', 'SceneEvent', 'TimelineEvent', 'Timeline',
+  'Compositor', 'Scene', 'Manuscript', 'ShortStory',
+  'elvn',
+]
--- src/Experiments/VoiceOver/__main__.py
+++ src/Experiments/VoiceOver/__main__.py
@@ -4,9 +4,10 @@ The Entrypoint for the Package
 
 """
 from __future__ import annotations
-import logging, sys, os, asyncio, traceback, threading, signal, types
-from typing import NoReturn, Literal
-from collections.abc import Iterable, Mapping, Coroutine, Callable
+from typing import NoReturn, Literal, BinaryIO
+from collections.abc import AsyncGenerator, Iterable, Mapping, Coroutine, Callable, ByteString
+import logging, sys, os, asyncio, threading, signal, types, json, hashlib, pathlib
+from dataclasses import dataclass, field
 from collections import deque
 
 ### Package Imports
@@ -17,8 +18,124 @@ logger = logging.getLogger(__name__)
 
 async def run(env: Mapping[str, str], quit_event: asyncio.Event) -> retry_action_t:
   cfg = VoiceOver.load_cfg_from_env(env)
-  await VoiceOver.run(cfg, quit_event)
+  await render_example_manuscript(cfg)
   return 'stop'
+
+async def render_example_manuscript(cfg: VoiceOver.Cfg):
+  ### Setup consumables
+  voices_cache_file = cfg['WorkCache'] / 'voices.json'
+  logger.debug('Establishing an API Session w/ ElevenLabs')
+  async with VoiceOver.elvn.api_session(
+    api_key=cfg['ElevenLabsAPIKey'],
+  ) as api_session:
+    if voices_cache_file.exists():
+      logger.debug('Loading the ElevenLabs Voices Cache')
+      voices = json.loads(voices_cache_file.read_bytes())
+    else:
+      logger.debug('Caching the ElevenLabs Voices')
+      voices = await VoiceOver.elvn.voices(api_session)
+      voices_cache_file.write_bytes(json.dumps(voices).encode())
+ 
+    ### Generate the Runtime Tools Protocol
+    @dataclass
+    class RuntimeTools:
+
+      ### Caching
+      (cfg['WorkCache'] / 'rt-cache').mkdir(exist_ok=True)
+      cache: pathlib.Path = field(default=cfg['WorkCache'] / 'rt-cache')
+      def key(self, *k: str) -> str: return hashlib.md5('|'.join(k).encode()).hexdigest()
+      def exists(self, *k: str) -> bool: return (self.cache / self.key(*k)).exists()
+      def slurp(self, *k: str) -> bytes: return (self.cache / self.key(*k)).read_bytes()
+      def dump(self, v: bytes, *k: str): (self.cache / self.key(*k)).write_bytes(v)
+      async def load(self, *k: str, chunk_size: int = VoiceOver.CHUNK_SIZE) -> AsyncGenerator[BinaryIO, None]:
+        with (self.cache / self.key(*k)).open('rb') as f:
+          while (c := f.read(chunk_size)): yield c
+      async def save(self, *k: str, src: AsyncGenerator[ByteString, None]):
+        with (self.cache / self.key(*k)).open('wb') as f:
+          async for c in src: f.write(c)
+
+      ### Tool Protocol
+      async def dub(self, text: str, actor: str, sink: BinaryIO | None = None) -> bytes | None:
+        
+        ### Cache Results on a Cache Miss
+        if not self.exists(text, actor): self.save(text, actor, (
+          await VoiceOver.elvn.tts(text, voices[actor], api_session, chunk_size=VoiceOver.CHUNK_SIZE)
+        ))
+        assert self.exists(text, actor)
+
+        ### Load the Results from Cache
+        if sink is None: return self.slurp(text, actor)
+        else:
+          async for c in self.load(text, actor):
+            sink.write(c)
+
+    TOOLS = RuntimeTools()
+    
+    ### Build the Scene
+
+    actors = {
+      n: VoiceOver.Actor(n) for n in (
+        'Lily', 'Roger', 'Callum'
+      )
+    }
+    roles = {
+      n: VoiceOver.Role(n) for n in (
+        'Narrator', 'Protagonist', 'Antagonist'
+      )
+    }
+    castings = VoiceOver.Castings(
+      roles=set(roles.values()),
+      actors=set(actors.values()),
+      castings={
+        roles[r]: actors[a]
+        for r, a in {
+          'Narrator': 'Lily',
+          'Protagonist': 'Roger',
+          'Antagonist': 'Callum',
+        }.items()
+      }
+    )
+
+    def compositor_factory(roles: set[VoiceOver.Role]) -> VoiceOver.Compositor:
+      compositor = VoiceOver.Compositor()
+      for role in roles:
+        assert isinstance(role.name, str), role.name
+        compositor.add_layer(role.name)
+      return compositor
+
+    intro_factory: Callable[[str], str] = lambda k: f'Hello! My name is {castings.actor_by_role(k).name} and I play the role of {castings[k].name}.'
+
+    manuscript = VoiceOver.Manuscript().add_act().add_scene(
+      'Example', 1, VoiceOver.Scene(
+        name='Example',
+        castings=castings,
+        chronology=[
+          set( VoiceOver.SceneEvent(when=when, **e) for e in events ) for when, events in enumerate((
+            (
+              { 'role': castings['Narrator'], 'voice_line': 'Scene Start.' },
+            ),
+            (
+              { 'role': castings['Narrator'], 'voice_line': intro_factory('Narrator') },
+            ),
+            (
+              { 'role': castings['Protagonist'], 'voice_line': intro_factory('Protagonist') },
+              { 'role': castings['Antagonist'], 'voice_line': intro_factory('Antagonist') },
+            ),
+            (
+              { 'role': castings['Narrator'], 'voice_line': 'Scene End.' },
+            )
+          ))
+        ],
+        compositor=compositor_factory(castings.roles),
+        tools=TOOLS,
+    ))
+
+    ### Render the Manuscript
+    
+    manuscript_workdir = cfg['WorkCache'] / 'manuscript'
+    manuscript_workdir.mkdir(exist_ok=True)
+    logger.info(f'Rendering Manuscript under {manuscript_workdir.as_posix()}')
+    await manuscript.render(manuscript_workdir)
 
 ### Runtime Boilerplate ###
 
